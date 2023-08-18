446601412d2df66d967edb30718c6c42af7709f0
alucard720 <ing.santana36@gmail.com>
2023-08-18 07:17:46 -0400

    client upload

---
+++ client/src/features/user/LandingIntro.js
@@ -0,0 +1,34 @@
+import TemplatePointers from "./components/TemplatePointers";
+import PRM from "../../pages/PRM.png";
+
+function LandingIntro() {
+  return (
+    <div className="hero min-h-full rounded-l-xl bg-base-200">
+      <div className="hero-content py-12">
+        <div className="max-w-md">
+          <h1 className="text-3xl text-center font-bold ">
+            <img
+              src={PRM}
+              className="w-12 inline-block mr-2 mask mask-circle"
+              alt="PRM-logo"
+            />
+            PRM DASHBOARD
+          </h1>
+
+          <div className="text-center mt-12">
+            <img
+              src={PRM}
+              alt="Dashwind Admin Template"
+              className="w-48 inline-block"
+            ></img>
+          </div>
+
+          {/* Importing pointers component */}
+          <TemplatePointers />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LandingIntro;
